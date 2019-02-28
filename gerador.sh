7d4a42f27f328fb9cfaa15b7ffe99510272a7374
Julio Lira <jul10l1r4@ufrn.edu.br>
2019-02-28 00:34:40 -0300

    Update gerador.sh

--- gerador.sh
+++ gerador.sh
@@ -7,6 +7,8 @@ printf """
 # Mente
 Loucuras, pensamentos poesias, tudo solto pelos diretórios, apenas em markdown, deve ser acessado por navegador CLI
 
+![A pobreza não é um acidente. Assim como a escravização e o Apartheid, a pobreza foi criada pello homem e pode ser removida pelas ações dos seres humanos](https://static.frasesparaface.com.br/imagem/a/p/a-pobreza-nao-e-um-acidente-assim-como-a.jpg)
+
 # Textos
 $links
 """ > README.md
